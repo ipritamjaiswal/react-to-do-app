87591afb17ff586e1465339204d3c134e9c499b5
pritamjaiswal.me <pritamjaiswal.me@gmail.com>
2024-09-25 13:43:44 +0000

    did tak form

---
+++ to-do-app/src/components/TaskForm.js
@@ -0,0 +1,29 @@
+import { useState, useContext } from "react";
+import { TaskContext, taskContext } from "../context/TaskContext";
+
+const TaskForm = () => {
+    const [title, setTitle] = useState("");
+    const { addTask } = useContext(TaskContext);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (title) {
+            addTask(title);
+            setTitle(''); // Clear the form
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input 
+            type="text" 
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            placeholder="Add a new task"
+            />
+            <button type="submit">Add Task</button>
+        </form>
+    );
+};
+
+export default TaskForm;
