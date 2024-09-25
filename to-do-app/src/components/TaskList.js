f345cca04691be46b9c77f2523ddbd8bca38300a
pritamjaiswal.me <pritamjaiswal.me@gmail.com>
2024-09-25 13:22:11 +0000

    did taskList

---
+++ to-do-app/src/components/TaskList.js
@@ -0,0 +1,17 @@
+import { useContext } from 'react';
+import { TaskContext } from '../context/TaskContext';
+import TaskItem from './TaskItem';
+
+const TaskList = () => {
+    const { tasks } = useContext(TaskContext);
+
+    return (
+        <div>
+            {tasks.map((task, index) => (
+                <TaskItem key={task.id} task={task} index={index} />
+            ))}
+        </div>
+    );
+};
+
+export default TaskList;
