a2d7c10bc68d099a7f728be4ddf663ebe743146c
pritamjaiswal.me <pritamjaiswal.me@gmail.com>
2024-09-25 13:46:37 +0000

    Adding app

--- to-do-app/src/App.js
+++ to-do-app/src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import TaskList from "./components/TaskList";
+import TaskForm from "./components/TaskForm";
+import { TaskProvider } from "./context/TaskContext"; 
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <TaskProvider>
+      <div className="App">
+        <TaskForm />
+        <TaskList />
+      </div>
+    </TaskProvider>
   );
 }
 
